0a5eb8648f700a28e2b0d090b76f793c9064c97d
Miklesami <mikle.samarkin@gmail.com>
2020-12-09 23:12:34 +0300

    add search with food

--- app/src/main/java/com/miklesam/applicationbeerapi/FragmentCategotyBeer.kt
+++ app/src/main/java/com/miklesam/applicationbeerapi/FragmentCategotyBeer.kt
@@ -1,10 +1,15 @@
 package com.miklesam.applicationbeerapi
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
+import androidx.paging.LoadState
 import com.miklesam.applicationbeerapi.databinding.FragmentCategoryBinding
 import com.miklesam.applicationbeerapi.databinding.FragmentRandomBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -24,19 +29,65 @@ class FragmentCategotyBeer : Fragment(R.layout.fragment_category) {
 
         binding.apply {
             catecoryRecycler.setHasFixedSize(true)
+            catecoryRecycler.itemAnimator = null
             catecoryRecycler.adapter = adapter.withLoadStateHeaderAndFooter(
-                header = BeerLoadStateAdapter{
+                header = BeerLoadStateAdapter {
                     adapter.retry()
                 },
-                footer = BeerLoadStateAdapter{
+                footer = BeerLoadStateAdapter {
                     adapter.retry()
                 }
             )
+            categoryRetry.setOnClickListener {
+                adapter.retry()
+            }
         }
 
         viewModel.beers.observe(viewLifecycleOwner) {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
 
+        adapter.addLoadStateListener { loadState ->
+            binding.apply {
+                categoryProgress.isVisible = loadState.source.refresh is LoadState.Loading
+                catecoryRecycler.isVisible = loadState.source.refresh is LoadState.NotLoading
+                categoryRetry.isVisible = loadState.source.refresh is LoadState.Error
+                textWithError.isVisible = loadState.source.refresh is LoadState.Error
+
+                if (loadState.source.refresh is LoadState.NotLoading &&
+                    loadState.append.endOfPaginationReached &&
+                    adapter.itemCount < 1
+                ) {
+                    catecoryRecycler.isVisible = false
+                    textEmpty.isVisible = true
+                } else {
+                    textEmpty.isVisible = false
+                }
+            }
+        }
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_category, menu)
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let {
+                    binding.catecoryRecycler.scrollToPosition(0)
+                    viewModel.searchCategoryNeers(query)
+                    searchView.clearFocus()
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+        })
     }
 }
